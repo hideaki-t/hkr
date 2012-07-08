fa23c4ba2472c2ba6a51616047a795a181556389
Hideaki Takahashi <mymelo@gmail.com>
2012-07-09 07:08:55 +0900

    throw away domain list and inspect contents to support a custom domain on Hatena Blog pro.

--- myscript.js
+++ myscript.js
@@ -1,4 +1,18 @@
 (function(){
+  var targets = {
+   "hatena.ne.jp":1,
+   "hatelabo.jp":1,
+   "hatenablog.com":1,
+   "hateblo.jp":1,
+   "hatenadiary.com":1,
+   "hatenadiary.jp":1
+  };
+  if (!(location.hostname.replace(/^[^.]+./, '') in targets)) {
+    var h = document.documentElement.attributes["data-blog"];
+    if (!h || !(h.value.replace(/^[^.]+./, '') in targets)) {
+      return;
+    }
+  }
   var r = document.createRange();
   var a = document.querySelectorAll("span.highlight,a.keyword,a.okeyword");
   var n = a.length;
