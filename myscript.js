9554613b0ea27a448db0cb3bc5a8bf2562261252
Hideaki Takahashi <mymelo@gmail.com>
2023-10-29 02:49:04 -0400

    partial rewrite

--- myscript.js
+++ myscript.js
@@ -1,24 +1,11 @@
-(function(){
-  var targets = {
-   "hatena.ne.jp":1,
-   "hatelabo.jp":1,
-   "hatenablog.com":1,
-   "hateblo.jp":1,
-   "hatenadiary.com":1,
-   "hatenadiary.jp":1
-  };
-  if (!(location.hostname.replace(/^[^.]+./, '') in targets)) {
-    var h = document.documentElement.attributes["data-blog"];
-    if (!h || !(h.value.replace(/^[^.]+./, '') in targets)) {
-      return;
-    }
-  }
-  var r = document.createRange();
-  var a = document.querySelectorAll("span.highlight,a.keyword,a.okeyword");
-  var n = a.length;
-  while (n--) {
-    var v = a[n];
+const reg = /^hate(?:na(?:\.ne\.jp|blog\.com|diary\.(?:com|jp))|labo\.jp|blo\.jp)/;
+if (
+  reg.test(location.hostname.replace(/^[^.]+./, "")) ||
+  reg.test(document.documentElement.attributes["data-blog"].value.replace(/^[^.]+./, ""))
+) {
+  const r = document.createRange();
+  document.querySelectorAll("span.highlight,a.keyword,a.okeyword").forEach((v) => {
     r.selectNodeContents(v);
     v.parentNode.replaceChild(r.extractContents(), v);
-  }
-})();
+  });
+}
